de85ea5de33964e5fe85c31d9ed1d1cb8df528d7
Justin <304602555@qq.com>
2021-02-21 12:40:22 +0800

    c

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"time"
 	"wechat_server/api"
-	"wechat_server/pkg/etcd"
 	"wechat_server/store"
 )
 
@@ -15,7 +14,7 @@ func main() {
 		panic(err)
 	}
 	log := new(store.Log)
-	go etcd.Register()
+	//go etcd.Register()
 	go api.GrpcServer()
 	log.Get().Debug("server started at %v", time.Now())
 	fmt.Printf("server started at %v \n", time.Now())
